2c77c42af51cc41e3d389f123a02cb3d535ca0a4
spectrum128 <spectrum128@kolorcloud.com>
2024-04-15 13:41:17 +0100

    Updated tcp_proxy - converted hexdump to python 3

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -6,15 +6,21 @@ import threading
 # hex dumping function taken from http://code.activestate.com/recipes/142812-hex-dumper/
 def hexdump(src, length=16):
     result = []
-    digits = 4 if isinstance(src, unicode) else 2
+    digits = 4 if isinstance(src, str) else 2
 
-    for i in xrange(0, len(src), length):
+    for i in range(0, len(src), length):
         s = src[i:i+length]
-        hexa = b" ".join(["%0*X" % (digits, ord(x)) for x in s])
-        text = b"".join([x if 0x20 <= ord(x) < 0x7F else b"." for x in s])
-        result.append(b"%04X %-*s %s" % (i, length*(digits + 1), hexa, text))
 
-    print(b"\n".join(result))
+        if isinstance(s, str):
+            hexa = " ".join(f"{ord(x):0{digits}X}" for x in s)
+            text = "".join(x if 0x20 <= ord(x) < 0x7F else "." for x in s)
+        else:
+            hexa = " ".join(f"{x:0{digits}X}" for x in s)
+            text = "".join(chr(x) if 0x20 <= x < 0x7F else "." for x in s)
+
+        result.append(f"{i:04X} {hexa:<{length*(digits + 1)}} {text}")
+        
+    print("\n".join(result))
 
 
 def receive_from(connection):
